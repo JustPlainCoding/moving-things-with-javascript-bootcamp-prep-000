61c3d32778815513161afaf5848925c2ae916708
Eric J Wilhite <ericjwilhite@gmail.com>
2019-05-24 04:02:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,7 +22,5 @@ function moveDodgerRight() {
   var rightNumbers = 
   dodger.style.right.replace('px', '');
   var right = parseInt(rightNumbers, 10);
-  if(right < 0) {
-    dodger.style.right = `${right + 1}px`;
-  }
+  console.log(right);
 }
